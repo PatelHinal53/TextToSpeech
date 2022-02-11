aed8b8d3b737ec4dfbe83689388678acf2e49a76
hinal.patel <hinal.patel@1R-79>
2022-02-11 15:24:58 +0530

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using Android.Support.V4.App;
+using Android.Speech.Tts;
+using Java.Util;
+
+namespace TextToSpeechDemo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity,TextToSpeech.IOnInitListener
+    {
+        private Button btnSpeak;
+        private EditText edtSpeak;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            SetContentView(Resource.Layout.activity_main);
+            
+            btnSpeak = FindViewById<Button>(Resource.Id.btnSpeak);
+            edtSpeak = FindViewById<EditText>(Resource.Id.edtSpeak);
+
+            btnSpeak.Click += BtnSpeak_Click;
+            
+        }
+    }
+}
